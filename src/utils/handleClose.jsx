76b43cf2ed7ac6123c58e015fbc69c17737994eb
feuvpi <fredvpg@gmail.com>
2022-11-03 18:29:01 -0300

    improving handleClose operation. It was causing a issue where the operation modal didnt open with the edit button click.

--- src/utils/handleClose.jsx
+++ src/utils/handleClose.jsx
@@ -3,20 +3,23 @@ import React, { useState } from "react";
 
  export const handleClose = (e, show) => {
     
-    while(show === false){
-        if(e.target.id !== 'addOperation' && 'editOperation') {
-            console.log('aqui')
-            return false
-        } else {
-            return true
-        }
-    }
-    while(show !== false){
+    // while(show === false){
+    //     if((e.target.farthestViewportElement.id) && e.target.farthestViewportElement.id !== 'addOperation' && 'editOperation') {
+    //         console.log(e.target.farthestViewportElement.id)
+    //         return false
+    //     } else {
+    //         return true
+    //     }
+    // }
+    while(show !== false && e.target.id !== 'editOperation'){
         if(e.target.id !== 'Modal'){
             return false;
         } else {
             return true;
         }
+        
     }
+
+    return null
     
 }
